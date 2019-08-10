ed3c9d06a3177e4ee5780b04d45826d710a93109
Scott <cmoshadow@gmail.com>
2019-08-11 00:29:27 +0800

    2 more problems. finished Google recent high-refrequency top 50.

---
+++ src/leetcode/LC353_Design_Snake_Game.java
@@ -0,0 +1,79 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LC353_Design_Snake_Game {
+
+    Queue<Integer> snake;
+    int width, height;
+    int row, col;
+    int[][] food;
+    int foodIdx;
+    int score;
+
+    public LC353_Design_Snake_Game(int width, int height, int[][] food) {
+        this.width = width;
+        this.height = height;
+        this.food = food;
+        snake = new LinkedList<>();
+        row = 0;
+        col = 0;
+        foodIdx = 0;
+        score = 0;
+        snake.add(0);
+    }
+
+    public int move(String direction){
+        switch (direction){
+            case "U": row--; break;
+            case "D": row++; break;
+            case "L": col--; break;
+            case "R": col++; break;
+        }
+        if (row < 0 || row >= height || col < 0 || col > width) {
+            score = -1;
+            return score;
+        }
+        // remove end of snake first, then detect head clash into body
+        if (foodIdx < food.length && food[foodIdx][0] == row && food[foodIdx][1] == col) {
+            foodIdx++;
+            score++;
+        }else {
+            snake.remove();
+        }
+        if (snake.contains(row * width + col)) {
+            score = -1;
+            return score;
+        }else{
+            snake.add(row * width + col);
+        }
+        return score;
+    }
+
+    private void display() {
+        char[][] canvas = new char[height][width];
+        if (foodIdx < food.length)
+            canvas[food[foodIdx][0]][food[foodIdx][1]] = 'F';
+        for (int i : snake) canvas[i / width][i % width] = 'S';
+        for (char[] row : canvas){
+            for (char c : row)
+                System.out.print(c);
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] arsg){
+        int[][] f = {{1,2},{0,1},{1,1}};
+        LC353_Design_Snake_Game s = new LC353_Design_Snake_Game(3, 2, f);
+        if (s.move("R") != -1) s.display();
+        if (s.move("D") != -1) s.display();
+        if (s.move("R") != -1) s.display();
+        if (s.move("U") != -1) s.display();
+        if (s.move("L") != -1) s.display();
+        if (s.move("D") != -1) s.display();
+        if (s.move("R") != -1) s.display();
+        if (s.move("U") != -1) s.display();
+    }
+}
---
+++ src/leetcode/LC616_Add_Bold_Tag_in_String.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LC616_Add_Bold_Tag_in_String {
+    public String addBoldTag(String s, String[] dict) {
+        List<int[]> sorted = new ArrayList<>(); //i[1] is length
+        for (String d : dict) sorted.add(new int[]{s.indexOf(d), d.length()});
+        sorted = sorted.stream().filter(i -> i[0] > -1).sorted(Comparator.comparing(i -> i[0])).collect(Collectors.toList());
+
+        List<int[]> merged = new ArrayList<>(); //i[1] is end point
+        int[] prev = sorted.get(0);
+        int[] prevInterval = new int[]{prev[0], prev[0] + prev[1]};
+        merged.add(prevInterval);
+        for (int[] interval : sorted) {
+            int end = interval[0] + interval[1];
+            if (interval[0] > prevInterval[1]) {
+                int[] curr = new int[]{interval[0], end};
+                merged.add(curr);
+                prevInterval = curr;
+            }else{
+                prevInterval[1] = Math.max(prevInterval[1], end);
+            }
+        }
+
+        for (int i = merged.size() - 1; i >= 0; i--) {
+            int[] interval = merged.get(i);
+            s = s.substring(0, interval[0]) + "<b>" + s.substring(interval[0], interval[1]) + "</b>" + s.substring(interval[1]);
+        }
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        LC616_Add_Bold_Tag_in_String sol = new LC616_Add_Bold_Tag_in_String();
+        System.out.println(sol.addBoldTag("aaabbcc", new String[]{"aaa","aab", "abc","bc"}));
+    }
+}
